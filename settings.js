d11749977f2f4169b3b74929b897cd275d74fc68
w5-test-1 <128934932+w5-test-1@users.noreply.github.com>
2023-03-26 20:06:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['917736807522'] //ur owner number
+global.ownername = "4BDU" //ur owner name
+global.ytname = "YT: W5 SONGS" //ur yt chanel name
+global.socialm = "GitHub: WH173 5P1D3R" //ur github or insta name
+global.location = "India, Kerala, Kochi" //ur location
 
 //bot bomdy 
-global.ownernomer = "916909137213" //ur number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot MD V6' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.themeemoji = "🦄" //ur theme emoji
+global.ownernomer = "917736807522" //ur number
+global.premium = ['917736807522'] //ur premium number
+global.botname = 'W5 BOT' //ur bot name
+global.linkz = "https://chat.whatsapp.com/" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/@w5songs" //ur website to be displayed
+global.botscript = '' //script link
+global.themeemoji = "🐱" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.author = "W5-BOT" //ur sticker watermark author
+global.wm = "W5 BOT" //ur watermark
 
 // Other
 global.sessionName = 'session'
